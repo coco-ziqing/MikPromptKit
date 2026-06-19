e47e8203b0b045102416e0d21309c88fa5626485
PromptKit Developer <promptkit@local.dev>
2026-06-19 22:56:36 +0800

    cleanup: 删除临时检测脚本

--- _check_html.py
+++
@@ -1,5 +0,0 @@
-import urllib.request, re
-r = urllib.request.urlopen('http://127.0.0.1:8081/')
-html = r.read().decode()
-scripts = re.findall(r'<script src="([^"]+)"', html)
-print('\n'.join(scripts))
